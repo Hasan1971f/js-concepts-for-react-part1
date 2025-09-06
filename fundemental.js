851aab144f990c4becb537d69ed174746d943709
Hasan <mdhasanurrahmanhasan101@gmail.com>
2025-09-06 16:25:08 +0600

    added a fundemantaljs

---
+++ fundemental.js
@@ -0,0 +1,255 @@
+// const fatherName = 'Fazlar rahman'
+// console.log(fatherName)
+
+
+// let sesson  = 'rainy'
+// sesson = 'winter'
+// console.log(sesson)
+
+
+// const student = 'Hasan'
+// console.log(student)
+
+// let table = 'wood'
+//  table = 'Plastic'
+// console.log( table)
+
+
+//  const constaction = 'Shihab'
+// console.log(constaction)
+
+//  let  School = 'goriArban'
+//  School = 'Shalea'
+//  console.log( School)
+
+// const myName = 'Hasan'
+// let age = '38'
+// age = '40'
+
+// console.log(myName, age )
+
+
+// const numbers = [89, 35, 98, 12]
+// for(let i = 0; i<numbers.length; i++){
+//    const number = numbers[i]
+//    console.log(number)
+// }
+
+// for(let i = 0; i<5; i++){
+//     console.log(i)
+// }
+// for(let i = 0; i<10; i++){
+//     console.log(i)
+// }
+// for(let i = 10; i>0; i++){
+//     console.log(i)
+// }
+
+// for (let i = 0; i < 10; i  += 3){
+//     console.log(i)
+// }
+
+// for (let i = 5; i > 0 ; i --){
+//     console.log(i)
+// }
+
+// function multiply(num1, num2) {
+//     const result = num1 * num2
+//     return result
+// }
+// const output = multiply(20 * 32)
+// console.log(output)
+// function greet (){
+//     console.log("Hello, Hasan!")
+// }
+// greet()
+
+// function name (){
+//     console.log('Rony')
+// }
+// name()
+
+// function student(){
+//     console.log('Apple')
+// }
+// student()
+// function add (a,b){
+//     return a + b
+// }
+// const result = add(2,4)
+// console.log(result)
+
+// function rahim (a,b){
+//     return a * b
+// }
+// const karim = 3 * 800
+// console.log(karim)
+
+// function number(a,b){
+//     return  a / b
+// }
+//  const box = 500 / 200
+//  console.log(box)
+
+// function help (a,b){
+//     return a * b
+// }
+// const number = 20000* 3000
+// console.log(number)
+
+// function number (a,b){
+//     return a * b
+// }
+
+// console.log(number(200000,5))
+
+// function hello (a,b){
+//     return a * b 
+// }
+// const output = 20000 * 3
+// console.log(output)
+
+// function help(a, b) {
+//     return a * b
+// }
+// console.log(help(5, 2))
+
+
+// function team (a, b){
+//     return a * b
+// }
+// const output = 20, 36
+// console.log(output)
+
+
+// function hello (a,b){
+//     return a * b 
+// }
+// const output = 20000 * 3
+// console.log(output)
+
+// function team (a , b){
+//     return a * b
+// }
+
+// const output = 20 * 63
+// console.log(output)
+
+
+// function team (a , b){
+//     return a * b
+// }
+
+
+// console.log(team(2 , 5))
+
+//  Final function--------------
+// function rony(a, b){
+//     return  65 * 32
+// }
+// const paper = 65 * 32
+// console.log(paper)
+
+
+// function hasan (a, b){
+//     return a * b
+// }
+
+// console.log(hasan( 20, 32))
+
+
+// for Lopp-------
+
+// for (let i = 0; i < 5; i++) {
+//     console.log(i)
+// }
+
+// for (let i = 0; i <= 10; i += 3) {
+//     console.log(i)
+// }
+
+
+// Basic Loop
+
+// for (let i =0; i< 10; i++){
+//     console.log(i)
+// }
+
+// for(let i =0 ; i<10; i +=2){
+//     console.log(i)
+// }
+
+// for (let i = 10; i>=0 ; i++){
+//     console.log(i)
+// }
+
+
+// again
+
+// for(let i = 0; i<10 ; i--){
+//     console.log(i)
+// }
+
+// for (let i = 0; i < 10; i += 2){
+//     console.log(i)
+// }
+
+// for(let i = 10; i>=0; i--){
+//     console.log(i)
+// }
+
+// for(let i = 0; i<10; i++){
+//     console.log(i)
+// }
+
+// for (let i = 0; i < 10; i -= 2) {
+//     console.log(i)
+// }
+
+// for(let i = 10; i>=0; i--){
+//     console.log(i)
+// }
+
+// function hello(a,b){
+//     return a * b
+// }
+// const almari = 20 * 45
+// console.log(almari)
+
+// function almari(a, b) {
+//     return a * b
+// }
+// console.log(almari(2, 5))
+// -------For Loop---------------
+
+// for(let i = 0; i<5 ; i++){
+//     console.log(i)
+// }
+
+// for(let i = 0; i<5; i += 2){
+//     console.log(i)
+// }
+
+// for(let i = 10; i>=0; i--){
+//     console.log(i)
+// }
+
+// function table(a, n){
+//     return a * n
+// }
+
+// console.log(table(2,4))
+
+// function mango(a, b) {
+//     return a * b
+// }
+// const apple = 2 * 4
+// console.log(apple)
+
+// Loop to print numbers 0 to 4
+// const n = 7; // তুমি চাইলে এখানে অন্য সংখ্যা দিতে পারো
+
+// const n = 7; 
+// for(let i = 0; i <= n; i++){
+//     console.log(i);
+// }
