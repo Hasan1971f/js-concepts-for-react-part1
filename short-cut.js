268f74bde99af7063a4c83e13975695c5138b84c
Hasan <mdhasanurrahmanhasan101@gmail.com>
2025-09-09 16:23:07 +0600

    added a short-cut.js

---
+++ short-cut.js
@@ -0,0 +1,17 @@
+let myVar = 5
+if (myVar) {
+    myVar = myVar * 100
+}
+else {
+    myVar = 0
+}
+
+const money = 80
+
+let food = money > 100 ? 'birayani' : 'cha Buscit'
+// console.log(food)
+
+
+const mony1 = 500
+let food1 = mony1 > 800 ? 'birahani' : 'vat'
+console.log(food1)
